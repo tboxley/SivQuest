ed7b3c1535aa43e1ab43ae8532f4d1ab0cd338c9
Travis Prue <travis.prue@gmail.com>
2014-01-18 01:52:23 -0600

    Organized images

--- json/images.php
+++ json/images.php
@@ -1,7 +1,7 @@
 <?php
   header('Content-type: text/javascript');
   $output=['imgs'=>[]];
-  foreach(glob("../img/*") as $v) {
+  foreach(glob("../img/{*.png,*/*.png}",GLOB_BRACE) as $v) {
     $output['imgs'][]=substr($v,7,-4);
   }
 
